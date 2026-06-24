5d3a397110e50f2ef3e6d1b3c104e36c7448fd17
Ilia Oberemok <i.oberemok777@gmail.com>
2026-06-24 07:55:04 +0300

    internal/auth: refactor the middleware a little -- handler malformed auth headers, allow for the extraction of value from context

--- internal/auth/middleware.go
+++ internal/auth/middleware.go
@@ -2,26 +2,43 @@ package auth
 
 import (
 	"context"
+	"errors"
 	"net/http"
 	"strings"
+
+	"github.com/golang-jwt/jwt/v5"
 )
 
 type contextKey string
 
 const claimsKey contextKey = "claims"
 
-func parseJWTToken(r *http.Request) string {
-	token := r.Header.Get("Authorization")
-	token = strings.Split(token, " ")[1]
-	return strings.TrimSpace(token)
+func parseJWTToken(r *http.Request) (string, error) {
+	headerValue := r.Header.Get("Authorization")
+
+	headerParts := strings.Split(headerValue, " ")
+	if len(headerParts) < 2 {
+		return "", errors.New("malformed authorization header")
+	}
+
+	token := strings.TrimSpace(headerParts[1])
+	return token, nil
+}
+
+func ClaimsFromContext(ctx context.Context) jwt.MapClaims {
+	return ctx.Value(claimsKey).(jwt.MapClaims)
 }
 
 func (ah *AuthHandler) AuthMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		jwtToken := parseJWTToken(r)
+		jwtToken, err := parseJWTToken(r)
+		if err != nil {
+			writeJSON(w, http.StatusBadRequest, map[string]string{"error": err.Error()})
+		}
+
 		claims, err := ah.validateJWT(jwtToken)
 		if err != nil {
-			writeJSON(w, http.StatusBadRequest, map[string]string{"error": "failed to verify the JWT"})
+			writeJSON(w, http.StatusUnauthorized, map[string]string{"error": "failed to verify the JWT"})
 			return
 		}
 		ctx := context.WithValue(r.Context(), claimsKey, claims)
