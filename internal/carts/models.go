435ab4ffe49d2359b35cf037f0d2923c86960179
Ilia Oberemok <i.oberemok777@gmail.com>
2026-06-25 08:11:38 +0300

    internal/carts: implement ListCartItems

--- internal/carts/models.go
+++ internal/carts/models.go
@@ -2,10 +2,19 @@ package carts
 
 import "github.com/inflame-ue/gocommerce/internal/database"
 
+// main cart handler
 type CartHandler struct {
 	db *database.DB
 }
 
 func NewCartHandler(db *database.DB) *CartHandler {
 	return &CartHandler{db: db}
+}
+
+// database models
+type CartItem struct {
+	ProductID int     `json:"product_id"`
+	Name      string  `json:"name"`
+	Price     float64 `json:"price"`
+	Quantity  int     `json:"quantity"`
 }
--- internal/carts/repository.go
+++ internal/carts/repository.go
@@ -1,1 +1,24 @@
 package carts
+
+import (
+	"context"
+	"fmt"
+)
+
+func (ch *CartHandler) ListCartItems(ctx context.Context, userID int) ([]CartItem, error) {
+	var item CartItem
+	var items []CartItem
+
+	rows, err := ch.db.Conn.Query(ctx, "SELECT cart_items.quantity, products.id, products.name, products.price FROM cart_items JOIN products ON carts_items.product_id WHERE user_id = $1", userID)
+	if err != nil {
+		return nil, fmt.Errorf("creating the query: %w", err)
+	}
+	for rows.Next() {
+		if err := rows.Scan(&item.Quantity, &item.ProductID, &item.Name, &item.Price); err != nil {
+			return nil, fmt.Errorf("scanning one of the rows: %w", err)
+		}
+		items = append(items, item)
+	}
+
+	return items, nil
+}
